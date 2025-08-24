c324820b5f5d1b011a3408e96b411c5129d4eae5
AryanV-Coder <aryanv.india1@gmail.com>
2025-08-24 10:17:18 +0530

    improved chat screen in app

--- spyai/lib/screens/chat.dart
+++ spyai/lib/screens/chat.dart
@@ -14,8 +14,38 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 3, 28, 48),
-      body: Center(
-        child: Text("CHAT WITH SPY", style: TextStyle(color: Colors.white)),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 500,
+                width: 500,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(71, 10, 21, 37),
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      blurRadius: 10,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text(
+                    'Chat Interface',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              )
+            ]
+          ),
+        ),
       ),
     );
   }
